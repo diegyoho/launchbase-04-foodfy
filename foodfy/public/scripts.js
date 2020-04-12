9c45c6af50e210b6ac60ed4c91f87487fc6b36fc
Diego Moreira <chicodiegomoreira@gmail.com>
2020-04-12 11:05:43 -0300

    Recipe Details style, control

--- foodfy/public/scripts.js
+++ foodfy/public/scripts.js
@@ -1,7 +1,15 @@
-const cards = document.querySelectorAll('.card')
+const details = document.querySelectorAll('#recipe-details .details')
 
-for (const card of cards) {
-    card.addEventListener('click', function () {
-        alert('Clicked!')
+for (const detail of details) {
+    const a = detail.querySelector('a')
+
+    a.addEventListener('click', function () {
+        if (detail.querySelector('.content').classList.contains('hidden')) {
+            a.innerText = 'ESCONDER'
+            detail.querySelector('.content').classList.remove('hidden')
+        } else {
+            a.innerText = 'MOSTRAR'
+            detail.querySelector('.content').classList.add('hidden')
+        }
     })
 }
